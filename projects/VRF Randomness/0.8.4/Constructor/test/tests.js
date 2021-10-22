48adcdef2af195efef71c4d56e29c080d14430d8
Daniel Nolan <DanNolan99@gmail.com>
2021-10-22 15:57:48 -0400

    Stage 1 Test Cases

--- projects/VRF Randomness/0.8.4/Constructor/test/tests.js
+++ projects/VRF Randomness/0.8.4/Constructor/test/tests.js
@@ -10,10 +10,13 @@ describe('Contract', function () {
             await contract.deployed();
         });
 
-        it('should have the right fee and keyhash', async () => {
+        it("should have the correct fee", async () => {
             const fee = await contract.fee();
             const expectedFee = ethers.utils.parseEther("2");
             assert(fee.eq(expectedFee), `Did not get the correct fee! It should 2 LINK, you provided: ${fee.toString()}`);
+        });
+
+        it('should have the correct keyhash', async () => {
             const keyhash = await contract.keyHash();
             const expectedKeyHash = "0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445";
             assert(keyhash.toLowerCase() === expectedKeyHash.toLowerCase());
@@ -25,9 +28,9 @@ describe('Contract', function () {
             const vrfIndex = bytecode.indexOf(expectedVrfCoordinator);
             const expectedLink = ("514910771AF9Ca656af840dff83E8264EcF986CA").toLowerCase();
             const linkIndex = bytecode.indexOf(expectedLink);
-            assert.isAtLeast(vrfIndex, 0);
-            assert.isAtLeast(linkIndex, 0);
-            assert.isAbove(linkIndex, vrfIndex);
+            assert(vrfIndex >= 0, "You did not deploy the correct Ethereum Mainnet VRF Coordinator Address.");
+            assert(linkIndex >= 0, "You did not deploy the correct LINK Mainnet Token Address.");
+            assert(linkIndex > vrfIndex, "It looks like you passed in the VRF Address and Link Token to the constructor in the wrong order.");
         });
     });
 });
