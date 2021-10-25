d2958945ecdbac2d662cfe14319b7287a7106a46
AlvaroLuken <al5031@nyu.edu>
2021-10-25 08:00:01 -0700

    Started code back in + hint!

--- projects/Data Feeds/0.8.4/Pick an Aggregator/setup/contract.sol
+++ projects/Data Feeds/0.8.4/Pick an Aggregator/setup/contract.sol
@@ -6,5 +6,9 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract PriceConsumerV3 {
     // ETH USD aggregator on the ETH Mainnet Chain
     AggregatorV3Interface public priceFeed = AggregatorV3Interface();
+
+    function getLatestPrice() external view returns (int) {
+
+    }
     
 }
